b6dee55ad834a34d5baf21eed2b609e795a4d0a1
Sooho <sooho.lim4@gmail.com>
2024-12-03 23:00:16 -0800

    i have no idea

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,14 +1,17 @@
-import {MongoClient} from "mongodb";
+import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI as string;
-const options = {};
 let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
 
-if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
     global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
 }
-
-const clientPromise = global._mongoClientPromise;
-
 export default clientPromise;
